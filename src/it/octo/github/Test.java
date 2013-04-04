dc2f26e6ca0449275d94aa6764388d3ddba59471
Giuseppe Libertucci <Giuseppe Libertucci@OCTO-Lap027>
2013-04-04 19:00:40 +0200

    zxczxczxc

--- src/it/octo/github/Test.java
+++ src/it/octo/github/Test.java
@@ -3,16 +3,7 @@ package it.octo.github;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test GITHUB repos - 5");
+		System.out.println("Test GITHUB repos - 6");
 	}
 }
-
-/* Git revision information:
-* $Id $
-* $Date $
-* $File $
-* $Author $
-* $Revision $
-* $Source $
-*/
 
