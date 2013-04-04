80a2b258d2b13f7d07987558c64251fddd193d94
Giuseppe Libertucci <Giuseppe Libertucci@OCTO-Lap027>
2013-04-04 14:55:11 +0200

    test keywords expansion

--- src/it/octo/github/Test.java
+++ src/it/octo/github/Test.java
@@ -3,7 +3,17 @@ package it.octo.github;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test GITHUB repos - 4");
+		System.out.println("Test GITHUB repos - 5");
 	}
 
 }
+
+/* Git revision information:
+* $Id$
+* $Date$
+* $File$
+* $Author$
+* $Revision$
+* $Source$
+*/
+
