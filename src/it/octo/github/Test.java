d66cc4e25f175eafa74f05ee30f5c6577897a251
Giuseppe Libertucci <Giuseppe Libertucci@OCTO-Lap027>
2013-04-04 14:45:36 +0200

    vai vai

--- src/it/octo/github/Test.java
+++ src/it/octo/github/Test.java
@@ -3,7 +3,7 @@ package it.octo.github;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test GITHUB repos - 2");
+		System.out.println("Test GITHUB repos - 4");
 	}
 
 }
